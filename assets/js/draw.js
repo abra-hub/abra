b78bc2fd953f1eed6b43ecb6a59c7a0a5852635f
maros.pekarik@pm.me <h8WX-}H9*Us:#4})})>
2021-02-25 10:25:40 +0100

    small updated to draw function

--- assets/js/draw.js
+++ assets/js/draw.js
@@ -3,40 +3,47 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	function getRandomRange(min, max) {
 		return Math.random() * (max - min) + min;
-	  }
+	}
 
-	var Agent = function(noiseZRange, index) {
-		var isEven = false
-		if(index % 2 == 0) {
-			isEven = true
+	class Agent {
+		constructor(noiseZRange, index) {
+			var isEven = false
+			if(index % 2 == 0) {
+				isEven = true
+			}
+			this.vector = myp5.createVector(isEven ? 0 : myp5.width, getRandomRange(0, myp5.height));
+			this.vectorOld = this.vector.copy();
+			this.stepSize = getRandomRange(1, 1.5);
+			this.angle;
+			this.noiseZ = getRandomRange(0, noiseZRange);
 		}
-		this.vector = myp5.createVector(isEven ? 0 : myp5.width, getRandomRange(0, myp5.height));
-		this.vectorOld = this.vector.copy();
-		this.stepSize = getRandomRange(1, 1.5);
-		this.angle;
-		this.noiseZ = getRandomRange(0, noiseZRange);
-	  };
+		update(strokeWidth, noiseZVelocity, mouseVec) {
+			// console.log(myp5.int(this.vector.dist(mouseVec)))
+			if(myp5.int(this.vector.dist(mouseVec)) < 100) {
+				strokeWidth = strokeWidth + 1.5
+			} else {
+				strokeWidth = strokeWidth
+			}
+			this.vector.x += Math.cos(this.angle) * this.stepSize;
+			this.vector.y += Math.sin(this.angle) * this.stepSize;
 
-	  Agent.prototype.update = function(strokeWidth, noiseZVelocity) {
-		this.vector.x += Math.cos(this.angle) * this.stepSize;
-		this.vector.y += Math.sin(this.angle) * this.stepSize;
+			if (this.vector.x < -10) this.vector.x = this.vectorOld.x = myp5.width + 10;
+			if (this.vector.x > myp5.width + 10) this.vector.x = this.vectorOld.x = -10;
+			if (this.vector.y < -10) this.vector.y = this.vectorOld.y = myp5.height + 10;
+			if (this.vector.y > myp5.height + 10) this.vector.y = this.vectorOld.y = -10;
 
-		if (this.vector.x < -10) this.vector.x = this.vectorOld.x = myp5.width + 10;
-		if (this.vector.x > myp5.width + 10) this.vector.x = this.vectorOld.x = -10;
-		if (this.vector.y < -10) this.vector.y = this.vectorOld.y = myp5.height + 10;
-		if (this.vector.y > myp5.height + 10) this.vector.y = this.vectorOld.y = -10;
+			myp5.strokeWeight(strokeWidth * this.stepSize);
+			myp5.line(this.vectorOld.x, this.vectorOld.y, this.vector.x, this.vector.y);
 
-		myp5.strokeWeight(strokeWidth * this.stepSize);
-		myp5.line(this.vectorOld.x, this.vectorOld.y, this.vector.x, this.vector.y);
+			this.vectorOld = this.vector.copy();
+			this.noiseZ += noiseZVelocity;
+		};
 
-		this.vectorOld = this.vector.copy();
-		this.noiseZ += noiseZVelocity;
-	  };
-
-	  Agent.prototype.update1 = function(strokeWidth, noiseScale, noiseStrength, noiseZVelocity) {
-		this.angle = myp5.noise(this.vector.x / noiseScale, this.vector.y / noiseScale, this.noiseZ) * noiseStrength;
-		this.update(strokeWidth, noiseZVelocity);
-	  };
+		update1(strokeWidth, noiseScale, noiseStrength, noiseZVelocity, mouseVec) {
+			this.angle = myp5.noise(this.vector.x / noiseScale, this.vector.y / noiseScale, this.noiseZ) * noiseStrength;
+			this.update(strokeWidth, noiseZVelocity, mouseVec);
+		};
+	}
 
 	var sketch = function(p) {
 		p.disableFriendlyErrors = true; // disables FES
@@ -52,6 +59,8 @@ document.addEventListener("DOMContentLoaded", function () {
 		var r = bgColor[0] * a + 255 * (1 - a)
 		var g = bgColor[1] * a + 255 * (1 - a)
 		var b = bgColor[2] * a + 255 * (1 - a)
+		var mouseVec = p.createVector(0,0)
+
 		p.setup = function() {
 		  p.createCanvas(p.windowWidth, p.windowHeight);
 		  p.colorMode(p.RGB, 255, 255, 255, 1);
@@ -67,12 +76,16 @@ document.addEventListener("DOMContentLoaded", function () {
 		  p.rect(0, 0, p.width, p.height);
 		  for (var i = 0; i < agentCount; i++) {
 			p.stroke(102, 116, 124, getRandomRange(0.1,1));
-			agents[i].update1(strokeWidth, noiseScale, noiseStrength, noiseZVelocity);
+			agents[i].update1(strokeWidth, noiseScale, noiseStrength, noiseZVelocity, mouseVec);
 		  }
 		};
 
 		p.windowResized = function(e) {
 			p.resizeCanvas(p.windowWidth, p.windowHeight);
+		}
+		p.mouseMoved = function(e) {
+			mouseVec.x = p.mouseX;
+			mouseVec.y = p.mouseY;
 		}
 
 		p.mouseWheel = function(e) {
